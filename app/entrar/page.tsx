759dbfe6e052c37e07c0b32f74e5bf74604f4f67
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-12-22 15:40:03 -0300

    fix: add suspense clause

--- app/entrar/page.tsx
+++ app/entrar/page.tsx
@@ -1,61 +1,70 @@
 'use client'
 
+import { Suspense } from 'react';
 import { useLogin } from './hook'
 
-export default function Home() {
-    const { register, handleSubmit, errors, onSubmit } = useLogin();
+const LoginForm = () => {
+  const { register, handleSubmit, errors, onSubmit } = useLogin();
 
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500">
-            <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-md">
-                <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Login</h2>
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500">
+      <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-md">
+        <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Login</h2>
 
-                {/* Exibindo erro geral */}
-                {errors.email || errors.password ? (
-                    <p className="text-red-500 text-center mb-4">Por favor, corrija os erros abaixo.</p>
-                ) : null}
+        {/* Exibindo erro geral */}
+        {errors.email || errors.password ? (
+          <p className="text-red-500 text-center mb-4">Por favor, corrija os erros abaixo.</p>
+        ) : null}
 
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    {/* Campo de Email */}
-                    <div className="mb-6">
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-600">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            {...register("email")}
-                            placeholder="Digite seu email"
-                            className={`w-full mt-2 p-3 border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300 ${errors.email ? "border-red-500" : "border-gray-300"
-                                }`}
-                        />
-                        {errors.email && (
-                            <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
-                        )}
-                    </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* Campo de Email */}
+          <div className="mb-6">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-600">Email</label>
+            <input
+              type="email"
+              id="email"
+              {...register("email")}
+              placeholder="Digite seu email"
+              className={`w-full mt-2 p-3 border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300 ${errors.email ? "border-red-500" : "border-gray-300"
+                }`}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
+            )}
+          </div>
 
-                    {/* Campo de Senha */}
-                    <div className="mb-6">
-                        <label htmlFor="password" className="block text-sm font-medium text-gray-600">Senha</label>
-                        <input
-                            type="password"
-                            id="password"
-                            {...register("password")}
-                            placeholder="Digite sua senha"
-                            className={`w-full mt-2 p-3 border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300 ${errors.password ? "border-red-500" : "border-gray-300"
-                                }`}
-                        />
-                        {errors.password && (
-                            <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>
-                        )}
-                    </div>
+          {/* Campo de Senha */}
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-600">Senha</label>
+            <input
+              type="password"
+              id="password"
+              {...register("password")}
+              placeholder="Digite sua senha"
+              className={`w-full mt-2 p-3 border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300 ${errors.password ? "border-red-500" : "border-gray-300"
+                }`}
+            />
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>
+            )}
+          </div>
 
-                    <button
-                        type="submit"
-                        className="w-full py-3 px-4 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300"
-                    >
-                        Entrar
-                    </button>
-                </form>
-            </div>
-        </div>
-    );
+          <button
+            type="submit"
+            className="w-full py-3 px-4 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+const Page = () => (
+  <Suspense>
+    <LoginForm />
+  </Suspense>
+)
+
+export default Page;
--- app/usuario/page.tsx
+++ app/usuario/page.tsx
@@ -2,135 +2,135 @@
 
 import React, { useState } from 'react';
 import {
-    Collapse,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    TablePagination,
-    IconButton
+	Collapse,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	TablePagination,
+	IconButton
 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { styled } from '@mui/material/styles';
 
 const CustomTableContainer = styled(TableContainer)({
-    boxShadow: '0px 13px 20px 0px #80808029',
-    borderRadius: '10px',
-    overflow: 'hidden',
+	boxShadow: '0px 13px 20px 0px #80808029',
+	borderRadius: '10px',
+	overflow: 'hidden',
 });
 
 interface RowData {
-    name: string;
-    email: string;
-    status: string;
+	name: string;
+	email: string;
+	status: string;
 }
 
 const data: RowData[] = [
-    { name: 'John Doe', email: 'john@example.com', status: 'Active' },
-    { name: 'Jane Smith', email: 'jane@example.com', status: 'Inactive' },
-    { name: 'Alice Johnson', email: 'alice@example.com', status: 'Pending' },
-    { name: 'Bob Brown', email: 'bob@example.com', status: 'Active' },
-    { name: 'John Doe', email: 'john@example.com', status: 'Active' },
-    { name: 'Jane Smith', email: 'jane@example.com', status: 'Inactive' },
-    { name: 'Alice Johnson', email: 'alice@example.com', status: 'Pending' },
-    { name: 'Bob Brown', email: 'bob@example.com', status: 'Active' },
-    { name: 'John Doe', email: 'john@example.com', status: 'Active' },
-    { name: 'Jane Smith', email: 'jane@example.com', status: 'Inactive' },
-    { name: 'Alice Johnson', email: 'alice@example.com', status: 'Pending' },
-    { name: 'Bob Brown', email: 'bob@example.com', status: 'Active' },
-    { name: 'John Doe', email: 'john@example.com', status: 'Active' },
-    { name: 'Jane Smith', email: 'jane@example.com', status: 'Inactive' },
-    { name: 'Alice Johnson', email: 'alice@example.com', status: 'Pending' },
-    { name: 'Bob Brown', email: 'bob@example.com', status: 'Active' },
+	{ name: 'John Doe', email: 'john@example.com', status: 'Active' },
+	{ name: 'Jane Smith', email: 'jane@example.com', status: 'Inactive' },
+	{ name: 'Alice Johnson', email: 'alice@example.com', status: 'Pending' },
+	{ name: 'Bob Brown', email: 'bob@example.com', status: 'Active' },
+	{ name: 'John Doe', email: 'john@example.com', status: 'Active' },
+	{ name: 'Jane Smith', email: 'jane@example.com', status: 'Inactive' },
+	{ name: 'Alice Johnson', email: 'alice@example.com', status: 'Pending' },
+	{ name: 'Bob Brown', email: 'bob@example.com', status: 'Active' },
+	{ name: 'John Doe', email: 'john@example.com', status: 'Active' },
+	{ name: 'Jane Smith', email: 'jane@example.com', status: 'Inactive' },
+	{ name: 'Alice Johnson', email: 'alice@example.com', status: 'Pending' },
+	{ name: 'Bob Brown', email: 'bob@example.com', status: 'Active' },
+	{ name: 'John Doe', email: 'john@example.com', status: 'Active' },
+	{ name: 'Jane Smith', email: 'jane@example.com', status: 'Inactive' },
+	{ name: 'Alice Johnson', email: 'alice@example.com', status: 'Pending' },
+	{ name: 'Bob Brown', email: 'bob@example.com', status: 'Active' },
 ];
 
 export default function Tabela() {
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(2);
-    const [selectedRow, setSelectedRow] = useState<number | null>(null);
+	const [page, setPage] = useState(0);
+	const [rowsPerPage, setRowsPerPage] = useState(2);
+	const [selectedRow, setSelectedRow] = useState<number | null>(null);
 
-    const handleChangePage = (e: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
-        e?.preventDefault()
-        setPage(newPage);
-    };
+	const handleChangePage = (e: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+		e?.preventDefault()
+		setPage(newPage);
+	};
 
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
+	const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setRowsPerPage(parseInt(event.target.value, 10));
+		setPage(0);
+	};
 
-    const handleRowClick = (index: number) => {
-        setSelectedRow(index === selectedRow ? null : index);
-    };
+	const handleRowClick = (index: number) => {
+		setSelectedRow(index === selectedRow ? null : index);
+	};
 
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 p-4">
-            <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-4xl">
-                <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Calçados disponíveis</h2>
-                <CustomTableContainer > {/* component={Paper} */}
-                    <Table>
-                        <TableHead>
-                            <TableRow className="bg-indigo-500 text-white">
-                                <TableCell className="text-white font-semibold">Name</TableCell>
-                                <TableCell className="text-white font-semibold">Email</TableCell>
-                                <TableCell className="text-white font-semibold">Status</TableCell>
-                                <TableCell className="text-white font-semibold">Actions</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => (
-                                <React.Fragment key={index}>
-                                    <TableRow className="hover:bg-indigo-100">
-                                        <TableCell>
-                                            <div
-                                                onClick={() => handleRowClick(index)}
-                                                className="cursor-pointer"
-                                            >
-                                                {row.name}
-                                            </div>
-                                        </TableCell>
-                                        <TableCell>{row.email}</TableCell>
-                                        <TableCell>{row.status}</TableCell>
-                                        <TableCell>
-                                            <IconButton onClick={() => handleRowClick(index)}>
-                                                <ExpandMoreIcon />
-                                            </IconButton>
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell colSpan={4} style={{ paddingBottom: 0, paddingTop: 0 }}>
-                                            <Collapse in={selectedRow === index} timeout="auto" unmountOnExit>
-                                                <div className="p-4">
-                                                    <p className="text-gray-700"><strong>Details:</strong></p>
-                                                    <p className="text-gray-700">Email: {row.email}</p>
-                                                    <p className="text-gray-700">Status: {row.status}</p>
-                                                </div>
-                                            </Collapse>
-                                        </TableCell>
-                                    </TableRow>
-                                </React.Fragment>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </CustomTableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[2, 5, 10]}
-                    component="div"
-                    count={data.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                    className="mt-4"
-                    classes={{
-                        root: 'flex justify-center',
-                        selectIcon: 'text-indigo-600',
-                        actions: 'text-indigo-600',
-                    }}
-                />
-            </div>
-        </div>
-    );
+	return (
+		<div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 p-4">
+			<div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-4xl">
+				<h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Calçados disponíveis</h2>
+				<CustomTableContainer > {/* component={Paper} */}
+					<Table>
+						<TableHead>
+							<TableRow className="bg-indigo-500 text-white">
+								<TableCell className="text-white font-semibold">Name</TableCell>
+								<TableCell className="text-white font-semibold">Email</TableCell>
+								<TableCell className="text-white font-semibold">Status</TableCell>
+								<TableCell className="text-white font-semibold">Actions</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => (
+								<React.Fragment key={index}>
+									<TableRow className="hover:bg-indigo-100">
+										<TableCell>
+											<div
+												onClick={() => handleRowClick(index)}
+												className="cursor-pointer"
+											>
+												{row.name}
+											</div>
+										</TableCell>
+										<TableCell>{row.email}</TableCell>
+										<TableCell>{row.status}</TableCell>
+										<TableCell>
+											<IconButton onClick={() => handleRowClick(index)}>
+												<ExpandMoreIcon />
+											</IconButton>
+										</TableCell>
+									</TableRow>
+									<TableRow>
+										<TableCell colSpan={4} style={{ paddingBottom: 0, paddingTop: 0 }}>
+											<Collapse in={selectedRow === index} timeout="auto" unmountOnExit>
+												<div className="p-4">
+													<p className="text-gray-700"><strong>Details:</strong></p>
+													<p className="text-gray-700">Email: {row.email}</p>
+													<p className="text-gray-700">Status: {row.status}</p>
+												</div>
+											</Collapse>
+										</TableCell>
+									</TableRow>
+								</React.Fragment>
+							))}
+						</TableBody>
+					</Table>
+				</CustomTableContainer>
+				<TablePagination
+					rowsPerPageOptions={[2, 5, 10]}
+					component="div"
+					count={data.length}
+					rowsPerPage={rowsPerPage}
+					page={page}
+					onPageChange={handleChangePage}
+					onRowsPerPageChange={handleChangeRowsPerPage}
+					className="mt-4"
+					classes={{
+						root: 'flex justify-center',
+						selectIcon: 'text-indigo-600',
+						actions: 'text-indigo-600',
+					}}
+				/>
+			</div>
+		</div>
+	);
 }
