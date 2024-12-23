d1b2b648fc2556b78e5e19ff3d175fe2b919befc
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-12-23 17:57:47 -0300

    .

--- app/painel/usuarios/page.tsx
+++ app/painel/usuarios/page.tsx
@@ -1,15 +1,19 @@
-// import Table from './_table'
-// import * as svc from '@/services'
+import Table from './_table'
+import * as svc from '@/services'
 
 const UserPage = async () => {
-  // const UsersPaginated = await svc.fetchUsersPaginated()
+  const UsersPaginated = await svc.fetchUsersPaginated()
 
   return (
     <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-4xl">
-      {/* <Table {...UsersPaginated} /> */}
+      <Table {...UsersPaginated} />
     </div>
   )
 }
 
-export default UserPage;
+const Page = () => (
+  <UserPage />
+)
+
+export default Page;
 
