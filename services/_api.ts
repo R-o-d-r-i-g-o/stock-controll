242e1012128101990c4d658796725282220eb80d
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-12-23 22:57:59 -0300

    feat: add request headers

--- services/_api.ts
+++ services/_api.ts
@@ -2,11 +2,13 @@
 import axios from 'axios'
 import { setupCache, cacheOperations } from './_cache'
 
+const _60_SECS = 1000 * 60;
 const _15_MINS = 1000 * 60 * 15;
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  maxRedirects: 1,
+  headers: { 'Content-Type': 'application/json' },
+  timeout: _60_SECS,
 });
 
 const cachedApi = setupCache(api, {
