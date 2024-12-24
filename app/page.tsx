2728007df7e255ea3b14f25c246f6f19686f143b
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-12-24 10:29:10 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,24 @@
-import { redirect } from 'next/navigation'
-import { getServerSession } from "next-auth/next";
-import { NavigationPage } from '@/common';
+// import { redirect } from 'next/navigation'
+// import { getServerSession } from "next-auth/next";
+// import { NavigationPage } from '@/common';
+
+import PwaComponent from '@/components/pwa'
 
 const Page = async () => {
-  const { Login, Home } = NavigationPage
-  const session = await getServerSession()
+  // const { Login, Home } = NavigationPage
+  // const session = await getServerSession()
 
-  const handleRedirect = () => {
-    if (session) {
-      redirect(Home)
-      return
-    }
+  // const handleRedirect = () => {
+  //   if (session) {
+  //     redirect(Home)
+  //     return
+  //   }
 
-    redirect(Login)
-  }
+  //   redirect(Login)
+  // }
 
-  handleRedirect()
-  return <></>
+  // handleRedirect()
+  return <PwaComponent />
 }
 
 export default Page;
