d2e270cc8af1c514091d465fdbc7cd48dacb8bc1
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-12-23 23:09:20 -0300

    .

--- services/_api.ts
+++ services/_api.ts
@@ -6,9 +6,10 @@ const _60_SECS = 1000 * 60;
 const _15_MINS = 1000 * 60 * 15;
 
 const api = axios.create({
+  timeout: _60_SECS,
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: { 'Content-Type': 'application/json' },
-  timeout: _60_SECS,
+  maxRedirects: 1,
 });
 
 const cachedApi = setupCache(api, {
