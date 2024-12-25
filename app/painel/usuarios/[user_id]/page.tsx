7e06ad594816d4a1bd63e45044eb2c4ae77c4bbd
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-12-25 10:27:26 -0300

    .

--- app/painel/usuarios/[user_id]/page.tsx
+++ app/painel/usuarios/[user_id]/page.tsx
@@ -1,13 +1,13 @@
 
 type UpdateUserPageProps = {
-    params: Promise<{
-        user_id: number;
-    }>
+  params: Promise<{
+    user_id: number;
+  }>
 }
 
 const UpdateUserPage = async ({ params }: UpdateUserPageProps) => {
 
-    return <>{JSON.stringify(await params)}</>
+  return <>{JSON.stringify(await params)}</>
 }
 
 export default UpdateUserPage;
--- app/painel/usuarios/page.tsx
+++ app/painel/usuarios/page.tsx
@@ -1,16 +1,24 @@
 import Table from './_table'
 import * as svc from '@/services'
 
-const UserPage = async () => {
+type UserListPageProps = {
+  searchParams: Promise<{
+    [key: string]: string | string[] | undefined
+  }>
+}
+
+const UserListPage = async ({ searchParams }: UserListPageProps) => {
+  const filters = (await searchParams).filters
   const UsersPaginated = await svc.fetchUsersPaginated()
+
+  console.log('veio aqui ', filters)
 
   return (
     <div className="bg-white p-6 sm:p-10 rounded-lg shadow-lg w-full max-w-4xl mx-5 sm:mx-0">
+      {JSON.stringify(filters)}
       <Table {...UsersPaginated} />
     </div>
   )
 }
 
-const Page = () => <UserPage />
-
-export default Page;
+export default UserListPage;
