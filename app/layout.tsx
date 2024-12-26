c715ebfb1c51a8a335d59e4a843bfefde4487340
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-12-26 19:17:57 -0300

    refactor: move text fonts to another director

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,18 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-import InfoButton from '@/components/info'
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import InfoButton from '@/components/info'
+
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../public/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../public/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
