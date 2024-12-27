da568706cb13f454a21f3fe2b0452a26319b9ad0
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-12-27 16:18:25 -0300

    .

--- app/painel/calcados/baixa/page.tsx
+++ app/painel/calcados/baixa/page.tsx
@@ -7,8 +7,8 @@ import { IconButton } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 
-// import Test from './barCodeScanner'
-import Test from './qrCodeScanner'
+import Test from './barCodeScanner'
+// import Test from './qrCodeScanner'
 
 const RegisterBuying = () => {
   const [tasks, setTasks] = useState<string[]>([]);
