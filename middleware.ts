1b306b45f0001bd82757d0c4343f6919eea9d605
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2025-01-03 20:58:48 -0300

    .

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
 export { default } from "next-auth/middleware";
 
 export const config = { matcher: ["/panel/:path*"] };
+
+export async function middleware(request: NextRequest) {
+  const sessionCookies = await cookies();
+  const authCookie = sessionCookies?.get("next-auth.session-token");
+
+  const requestHeaders = new Headers(request.headers);
+  if (authCookie)
+    requestHeaders.set("Cookie", `next-auth.session-token=${authCookie.value}`);
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+
+  return response;
+}
