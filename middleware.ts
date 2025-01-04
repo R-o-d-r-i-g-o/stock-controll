8484c2f8a5a56430531d2654b5d39ed82ab6ab47
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2025-01-03 22:15:59 -0300

    .

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,9 @@ export const config = { matcher: ["/panel/:path*"] };
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.JWT_SECRET! });
 
+  if (!token && req.nextUrl.pathname.startsWith("/panel"))
+    return NextResponse.redirect(new URL("/login", req.url));
+
   const requestHeaders = new Headers(req.headers);
   requestHeaders.set("user-email", token?.email ?? "");
 
