b0612fd04437f53d2a71f449da919c3a527a79b1
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2025-01-06 13:54:53 -0300

    .

--- common/jwt/index.ts
+++ common/jwt/index.ts
@@ -10,14 +10,9 @@ import * as svc from "@/backend";
 
 const secret = process.env.NEXTAUTH_SECRET;
 
-const cookieName =
-  process.env.VERCEL_ENV === "development"
-    ? "next-auth.session-token"
-    : "__Secure-next-auth.session-token";
-
 async function validateAuthUser(req: NextRequest) {
   const [token, session] = await Promise.all([
-    jwt.getToken({ req, secret, cookieName }),
+    jwt.getToken({ req, secret }),
     getServerSession(options),
   ]);
 
