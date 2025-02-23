4f3986b33ae4e408882adf6d325216ab00d1da47
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2025-02-23 16:49:29 -0300

    feat: implement code scanner selection properly

--- app/panel/shoes/debit/page.tsx
+++ app/panel/shoes/debit/page.tsx
@@ -9,15 +9,46 @@ import { CodeScanner } from "@/components/ui";
 
 import * as svc from "@/lib/services";
 
+const options = [
+  {
+    value: "register",
+    label: "Cadastrar Itens",
+  },
+  {
+    value: "debit",
+    label: "Baixa nos Itens",
+  },
+];
+
 const RegisterBuying = () => {
   const { failure, success } = useToast();
-  const [skus, setSkus] = useState<string[]>([]);
+  const [state, setState] = useState<{
+    skus: string[];
+    scannerActive: boolean;
+    oprationType: string;
+  }>({
+    skus: [],
+    scannerActive: false,
+    oprationType: "debit",
+  });
+
+  const handleActionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState((prevState) => ({
+      ...prevState,
+      oprationType: (event.target as HTMLInputElement).value,
+    }));
+  };
 
   const sendSKUs = async () => {
     try {
-      await svc.debitItemsFromStorage(skus);
-      success("SKUs debitados com sucesso!");
-      setSkus([]);
+      if (state.oprationType === "debit") {
+        await svc.debitItemsFromStorage(state.skus);
+        success("SKUs debitados com sucesso!");
+      } else {
+        // await svc.registerItemsToStorage(state.skus);
+        success("SKUs cadastrados com sucesso!");
+      }
+      setState((prevState) => ({ ...prevState, skus: [] }));
     } catch (error) {
       console.error(error);
       failure("Não foi possível concluir a requisição.");
@@ -25,7 +56,10 @@ const RegisterBuying = () => {
   };
 
   const handleRemoveSku = (sku: string) => {
-    setSkus(skus.filter((s) => s !== sku));
+    setState((prevState) => ({
+      ...prevState,
+      skus: prevState.skus.filter((s) => s !== sku),
+    }));
   };
 
   const handlOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -33,26 +67,54 @@ const RegisterBuying = () => {
     const form = new FormData(e.target as HTMLFormElement);
     const skuCode = form.get("sku")?.toString().trim() ?? "";
 
-    if (skuCode === "" || skus.includes(skuCode)) return;
-    setSkus([...skus, skuCode]);
+    if (skuCode === "" || state.skus.includes(skuCode)) return;
+    setState((prevState) => ({
+      ...prevState,
+      skus: [...prevState.skus, skuCode],
+    }));
     (e.target as HTMLFormElement).reset();
   };
 
-  const handleScanResult = useCallback((scannedSKU: string) => {
-    if (!skus || skus.includes(scannedSKU)) return;
-    setSkus((prevTasks) => [...prevTasks, scannedSKU]);
-  }, []);
+  const handleScanResult = useCallback(
+    (scannedSKU: string) => {
+      if (!state.skus || state.skus.includes(scannedSKU)) return;
+      setState((prevState) => ({
+        ...prevState,
+        skus: [...prevState.skus, scannedSKU],
+      }));
+    },
+    [state.skus]
+  );
 
   return (
     <div className="bg-white p-6 rounded-lg shadow w-full max-w-sm">
       <h2 className="text-xl font-medium text-center text-gray-800 mb-4">
-        SKUs para baixa
+        SKUs para {state.oprationType === "debit" ? "baixa" : "cadastro"}
       </h2>
-      <CodeScanner
-        beepEnabled
-        onResult={handleScanResult}
-        className="rounded-md mb-6"
-      />
+      <div className=" text-sm text-gray-600 mb-6">
+        <span className="block text-left">Operação</span>
+        <div className="flex justify-start gap-6 mt-2">
+          {options.map((rt) => (
+            <label key={rt.value} className="flex items-center space-x-2">
+              <input
+                type="radio"
+                value={rt.value}
+                checked={state.oprationType === rt.value}
+                onChange={handleActionChange}
+                className="text-indigo-600 focus:ring-indigo-500"
+              />
+              <span>{rt.label}</span>
+            </label>
+          ))}
+        </div>
+      </div>
+      {state.scannerActive && (
+        <CodeScanner
+          beepEnabled
+          onResult={handleScanResult}
+          className="rounded-md mb-6"
+        />
+      )}
       <form
         onSubmit={handlOnSubmit}
         className="flex items-center border-b border-gray-300 pb-2 mb-4"
@@ -68,7 +130,7 @@ const RegisterBuying = () => {
         </IconButton>
       </form>
       <ul className="divide-y divide-gray-200">
-        {skus.map((s) => (
+        {state.skus.map((s) => (
           <li
             key={s}
             className="flex justify-between items-center py-2 text-sm"
@@ -81,8 +143,16 @@ const RegisterBuying = () => {
         ))}
       </ul>
       <button
+        className="mt-4 w-full py-2 bg-blue-500 text-white text-sm rounded hover:bg-blue-600 transition"
+        onClick={() =>
+          setState((s) => ({ ...s, scannerActive: !s.scannerActive }))
+        }
+      >
+        {state.scannerActive ? "Desativar Scanner" : "Ativar Scanner"}
+      </button>
+      <button
         onClick={sendSKUs}
-        disabled={skus.length > 0}
+        disabled={state.skus.length === 0}
         className="mt-4 w-full py-2 bg-green-500 text-white text-sm rounded hover:bg-green-600 transition"
       >
         Enviar SKUs
