4d9cc20f80ba6c491cfdadcf3f7bb4539d45a557
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2025-03-04 14:03:06 -0300

    refactor: add improvements in aux pages

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { useEffect } from "react";
+import Link from "next/link";
+
+import Title from "@/components/ui/title";
+import Container from "@/components/templates/container";
 import Background from "@/components/templates/background";
 
 type ErrorPageProps = {
@@ -9,26 +12,44 @@ type ErrorPageProps = {
 };
 
 const ErrorPage = ({ error, reset }: ErrorPageProps) => {
-  useEffect(() => console.error(error), [error]);
-
   return (
     <Background type="error">
-      <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-md text-center">
-        <h2 className="text-5xl font-bold text-gray-800 mb-4">Ops...</h2>
-        <p className="text-xl font-medium text-gray-600 mb-6">
-          Desculpe, algo deu errado.
-        </p>
+      <Container className="text-center" display="small">
+        <Title className="mb-4 !text-5xl" text="Ops..." />
+        <Title className="mb-6 !text-xl" text=" Desculpe, algo deu errado" />
         <p className="text-gray-500 mb-8">
           Não conseguimos processar sua solicitação no momento. Tente novamente
           mais tarde ou entre em contato com o suporte.
         </p>
+        {error && (
+          <div className="bg-white p-2 rounded-lg shadow-md mb-6">
+            <Title className="!text-lg" text="Detalhes do erro:" />
+            <pre className="overflow-x-scroll text-sm text-red-600 mt-2">
+              {error.message}
+            </pre>
+            {error.stack && (
+              <div className="mt-4">
+                <h4 className="font-semibold text-gray-800">Stack Trace:</h4>
+                <pre className="overflow-x-scroll text-xs text-gray-500">
+                  {error.stack}
+                </pre>
+              </div>
+            )}
+          </div>
+        )}
         <button
           onClick={reset}
-          className="w-full py-3 px-4 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300"
+          className="w-full py-3 mb-6 px-4 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300"
         >
           Tente novamente
         </button>
-      </div>
+        <Link
+          href="/"
+          className="w-full py-3 px-4 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300"
+        >
+          Voltar para a Página Inicial
+        </Link>
+      </Container>
     </Background>
   );
 };
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,26 +1,25 @@
 import Link from "next/link";
 
-import { NavigationPage } from "@/common";
+import Title from "@/components/ui/title";
+import Container from "@/components/templates/container";
 import Background from "@/components/templates/background";
 
 const NotFoundPage = () => (
   <Background type="error">
-    <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-md text-center">
-      <h2 className="text-5xl font-bold text-gray-800 mb-4">404</h2>
-      <p className="text-xl font-medium text-gray-600 mb-6">
-        Não Encontrado(a)
-      </p>
+    <Container className="text-center" display="small">
+      <Title className="mb-4 !text-5xl" text="404" />
+      <Title className="mb-6 !text-xl" text="Não Encontrado(a)" />
       <p className="text-gray-500 mb-8">
         Desculpe, a página e/ou recurso que você está procurando não existe ou
         foi movido(a).
       </p>
       <Link
-        href={NavigationPage.Home}
+        href="/"
         className="w-full py-3 px-4 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300"
       >
         Voltar para a Página Inicial
       </Link>
-    </div>
+    </Container>
   </Background>
 );
 
