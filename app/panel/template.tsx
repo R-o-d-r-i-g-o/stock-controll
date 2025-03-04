530dc4141e2b97f4c52828674105439774d0c22e
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2025-03-04 12:04:12 -0300

    feat: implement page loading in template element

--- app/panel/template.tsx
+++ app/panel/template.tsx
@@ -1,4 +1,7 @@
+import React from "react";
+
 import Ping from "@/components/ui/ping";
+import Loader from "@/components/ui/loader";
 import Drawer from "@/components/ui/drawer";
 import Background from "@/components/templates/background";
 
@@ -9,7 +12,7 @@ type TemplateProps = {
 const Template = ({ children }: TemplateProps) => (
   <Background className="flex-col gap-6 p-4">
     <Drawer />
-    {children}
+    <React.Suspense fallback={<Loader />}>{children}</React.Suspense>
     <div className="hidden sm:block">
       <Ping />
     </div>
