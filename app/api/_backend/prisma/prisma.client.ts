479557b3df3325e44899966c7ef5fae09c61e71a
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2025-05-03 12:01:03 -0300

    chore: add prune command

--- app/api/_backend/prisma/prisma.client.ts
+++ app/api/_backend/prisma/prisma.client.ts
@@ -1,12 +1,8 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
-
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+});
 
 const prismaTransaction = async <T>(func: () => Promise<T>) => {
   await prisma.$connect();
