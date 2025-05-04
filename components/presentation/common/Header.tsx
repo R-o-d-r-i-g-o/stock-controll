2c6af70bf726d42b6c55b9cbe812357463cbbe22
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2025-05-03 22:49:58 -0300

    feat: add some more visual details

--- components/presentation/common/Header.tsx
+++ components/presentation/common/Header.tsx
@@ -8,7 +8,9 @@ interface IProps {
 const Header = ({ title, subtitle }: IProps) => (
   <div className="flex flex-col items-center gap-5 text-center">
     <p className="tracking-widest uppercase text-sm font-bold text-gray-300">{title}</p>
-    <p className="text-4xl md:text-[3.5rem] font-extrabold bg-gradient-to-r from-blue-600 via-indigo-700 to-purple-600 text-transparent bg-clip-text animate-pulse drop-shadow-[2px_2px_8px_rgba(255,255,255,0.9)]">{subtitle}</p>
+    <p className="text-4xl md:text-[3.5rem] font-extrabold bg-gradient-to-r from-blue-600 via-indigo-700 to-purple-600 text-transparent bg-clip-text animate-pulse drop-shadow-[2px_2px_8px_rgba(255,255,255,0.9)]" style={{ lineHeight: "1.2" }}>
+      {subtitle}
+    </p>
   </div>
 );
 
--- components/presentation/sections/SpecialOffer.tsx
+++ components/presentation/sections/SpecialOffer.tsx
@@ -2,22 +2,29 @@ import Link from "next/link";
 import React from "react";
 import Header from "../common/Header";
 import MainButton from "../common/MainButton";
+import LottieHandler from "../ui/lottie-handler";
+import carrierSuccess from "@/public/animations/carrier-success.json";
 
 function SpecialOffer() {
   return (
     <section id="oferta" className="mt-14 pt-4 bg-gray-50 rounded-lg shadow-md">
-      <div className="px-6 md:px-10 lg:px-16 py-8">
-        <Header title="Experimente Grátis!" subtitle="30 dias para um teste gratuíto" />
-        <div className="mt-8 md:mt-[3rem] text-center md:text-left">
-          <p className="mb-5 text-lg leading-relaxed text-gray-800">
-            Aproveite nossa <span className="font-semibold text-green-600">oferta especial de lançamento</span>! Adquira qualquer um de nossos planos e utilize o sistema ERP completo para sapatarias <span className="font-extrabold text-blue-600 text-xl">totalmente grátis por 30 dias</span>.
-          </p>
-          <p className="text-gray-700 mb-7 leading-relaxed">
-            Descubra como nosso software pode otimizar suas vendas, gerenciar seu estoque com eficiência e melhorar o relacionamento com seus clientes <span className="font-medium">sem nenhum custo inicial</span>.
-          </p>
-          <Link href="/register" className="mt-6 flex justify-center">
-            <MainButton text="Quero começar agora!" width="md:w-auto w-full" />
-          </Link>
+      <div className="px-6 md:px-10 lg:px-16 py-8 grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+        <div className="flex justify-center">
+          <LottieHandler lottie={carrierSuccess} height={300} maxWidth={300} minWidth={"60%"} />
+        </div>
+        <div className="text-center md:text-left">
+          <Header title="Experimente Grátis!" subtitle="30 dias para um teste gratuíto" />
+          <div className="mt-8 md:mt-[3rem]">
+            <p className="mb-5 text-lg leading-relaxed text-gray-800">
+              Aproveite nossa <span className="font-semibold text-green-600">oferta especial de lançamento</span>! Adquira qualquer um de nossos planos e utilize o sistema ERP completo para sapatarias <span className="font-extrabold text-blue-600 text-xl">totalmente grátis por 30 dias</span>.
+            </p>
+            <p className="text-gray-700 mb-7 leading-relaxed">
+              Descubra como nosso software pode otimizar suas vendas, gerenciar seu estoque com eficiência e melhorar o relacionamento com seus clientes <span className="font-medium">sem nenhum custo inicial</span>.
+            </p>
+            <Link href="/register" className="mt-6 flex justify-center md:justify-start">
+              <MainButton text="Quero começar agora!" width="md:w-auto w-full" />
+            </Link>
+          </div>
         </div>
       </div>
     </section>
