32c68c7bbf35ee75309669a8e2cbb7fd180febfc
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2025-05-04 11:13:36 -0300

    feat: add usability improvements

--- components/presentation/cards/TeamCard.tsx
+++ components/presentation/cards/TeamCard.tsx
@@ -1,38 +1,26 @@
 import React from "react";
-import { Separator } from "@/components/presentation/ui/separator";
 
-interface IProps {
-  imageUrl: string;
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+type TeamCardProps = {
   name: string;
+  imageUrl: string;
   profession: string;
-}
+};
 
-function TeamCard({ imageUrl, name, profession }: IProps) {
-  return (
-    <div>
-      <div className="flex flex-col justify-center items-center border-[4px] border-[#EBEAED] rounded-t-[1rem] py-[3rem]">
-        <div>
-          <img src={imageUrl} alt="team member photo" className="w-[7.5rem] h-[7.5rem] rounded-full" />
-        </div>
-        <p className="text-lightBlue font-bold text-[1.2rem] my-6">{name}</p>
-        <p className="text-darkBlue font-bold opacity-[0.3]">{profession}</p>
-      </div>
-
-      <div className="flex justify-around items-center h-[3.6875rem] border-[4px] border-[#EBEAED] border-t-0  rounded-b-[1rem]">
-        <div>
-          <img src="/images/x_icon.png" alt="social icon" />
-        </div>
-        <Separator orientation="vertical" color="#EBEAED" className="w-[3px]" />
-        <div>
-          <img src="/images/facebook_icon.png" alt="social icon" />
-        </div>
-        <Separator orientation="vertical" color="#EBEAED" className="w-[3px]" />
-        <div>
-          <img src="/images/dribble_icon.png" alt="social icon" />
-        </div>
-      </div>
+const TeamCard: React.FC<TeamCardProps> = ({ imageUrl, name, profession }) => (
+  <div className="flex flex-col gap-4 justify-center items-center border-2 border-white rounded-lg pt-[3rem] pb-4">
+    <img src={imageUrl} alt="team member photo" className="w-[7.5rem] h-[7.5rem] rounded-full" />
+    <p className="text-lightBlue font-bold text-[1.2rem]">{name}</p>
+    <p className="text-darkBlue font-bold opacity-[0.3]">{profession}</p>
+    <div className="flex justify-around items-center w-full">
+      <TwitterIcon />
+      <FacebookIcon />
+      <LinkedInIcon />
     </div>
-  );
-}
+  </div>
+);
 
 export default TeamCard;
